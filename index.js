19fe1b0dccfdf19fd43d8e52dfb2cc96ba51661b
RedPandaSmash <redpandasmash@gmail.com>
2025-06-23 12:36:57 -0400

    stuck

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-//Copy and paste your code from creating-json to start this project
-// Don't forget to copy your "api" directory and contents as well
+const express = require("express");
+const app = express();
+
+// define path
+const path = require("path");
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("api"));
+const cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "api", "answers.json"));
+});
